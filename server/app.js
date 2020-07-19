2ab60c698c1a5568e645546cd30dbcc80f9ecf1e
Glorison <lai.glorison@gmail.com>
2020-07-19 16:12:54 +1000

    scripts and stuff

--- server/app.js
+++ server/app.js
@@ -20,12 +20,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/open-weather', openWeatherRouter);
-app.use('/get-cities', citiesRouter);
-
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
+
+app.use('/open-weather', openWeatherRouter);
+app.use('/get-cities', citiesRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));

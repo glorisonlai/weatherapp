f6bab18c3d96154815d295605066c6ec9b897ac3
Glorison <lai.glorison@gmail.com>
2020-07-21 22:24:40 +1000

    ?

--- client/src/services/services.js
+++ client/src/services/services.js
@@ -6,7 +6,7 @@ const Services = {
   },
 
   prepareUrl(path) {
-    return window.location.protocol + '//' + window.location.hostname + ':' + process.env.PORT + path;
+    return window.location.protocol + '//' + window.location.hostname + ':' + process.env.REACT_APP_PORT + path;
   },
 
   async post(url, body) {

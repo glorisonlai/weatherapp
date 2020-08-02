a6abf61c0e4b8bf7c85e241fddc3d0bdda0df64b
Glorison <lai.glorison@gmail.com>
2020-08-02 21:22:34 +1000

    deploy to heroku

--- server/app.js
+++ server/app.js
@@ -18,9 +18,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static('client/build'));
+if (process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'));
+} else {
+  app.use(express.static(path.join(__dirname, 'public')));
+}
 
 app.use('/open-weather', openWeatherRouter);
 app.use('/get-cities', citiesRouter);

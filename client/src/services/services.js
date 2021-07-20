55c457778afae8cb0ea4eb6acfa6c6c1332ce893
Glorison <lai.glorison@gmail.com>
2021-07-20 12:36:19 +1000

    Fixed backend service

--- client/src/services/services.js
+++ client/src/services/services.js
@@ -7,7 +7,7 @@ const Services = {
 
   prepareUrl(path) {
     const { protocol, hostname } = window.location;
-    const PORT = process.env.PORT || 9000;
+    const PORT = process.env.PORT || 1337;
 
     return `${protocol}//${hostname}:${PORT}/${path}`;
   },
